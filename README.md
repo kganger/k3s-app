309b76ff0fbf5432a057e7c357dc3ff395d941da
Keith Ganger <keith.e.ganger@lmco.com>
2021-02-14 12:56:45 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+## Micronaut 2.3.2 Documentation
+
+- [User Guide](https://docs.micronaut.io/2.3.2/guide/index.html)
+- [API Reference](https://docs.micronaut.io/2.3.2/api/index.html)
+- [Configuration Reference](https://docs.micronaut.io/2.3.2/guide/configurationreference.html)
+- [Micronaut Guides](https://guides.micronaut.io/index.html)
+---
+
+## Feature graphql documentation
+
+- [Micronaut GraphQL documentation](https://micronaut-projects.github.io/micronaut-graphql/latest/guide/index.html)
+
+## Feature reactor documentation
+
+- [Micronaut Reactor documentation](https://micronaut-projects.github.io/micronaut-reactor/snapshot/guide/index.html)
+
+## Feature http-client documentation
+
+- [Micronaut HTTP Client documentation](https://docs.micronaut.io/latest/guide/index.html#httpClient)
+
+## Feature management documentation
+
+- [Micronaut Management documentation](https://docs.micronaut.io/latest/guide/index.html#management)
+
---
+++ build.gradle.kts
@@ -0,0 +1,75 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.4.10"
+    id("org.jetbrains.kotlin.kapt") version "1.4.10"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.4.10"
+    id("com.github.johnrengelman.shadow") version "6.1.0"
+    id("io.micronaut.application") version "1.3.4"
+    id("com.google.cloud.tools.jib") version "2.6.0"
+}
+
+version = "0.1"
+group = "com.example"
+
+val kotlinVersion=project.properties.get("kotlinVersion")
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+micronaut {
+    runtime("netty")
+    testRuntime("junit5")
+    processing {
+        incremental(true)
+        annotations("com.example.*")
+    }
+}
+
+dependencies {
+    implementation("io.micronaut:micronaut-validation")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${kotlinVersion}")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:${kotlinVersion}")
+    implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
+    implementation("io.micronaut:micronaut-runtime")
+    implementation("javax.annotation:javax.annotation-api")
+    implementation("io.micronaut:micronaut-http-client")
+    implementation("io.micronaut:micronaut-management")
+    implementation("io.micronaut.graphql:micronaut-graphql")
+    implementation("io.micronaut.kotlin:micronaut-kotlin-extension-functions")
+    implementation("io.micronaut.reactor:micronaut-reactor")
+    runtimeOnly("ch.qos.logback:logback-classic")
+    runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin")
+}
+
+
+application {
+    mainClass.set("com.example.ApplicationKt")
+}
+
+java {
+    sourceCompatibility = JavaVersion.toVersion("11")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions {
+            jvmTarget = "11"
+        }
+    }
+    compileTestKotlin {
+        kotlinOptions {
+            jvmTarget = "11"
+        }
+    }
+
+
+    jib {
+        to {
+            auth{
+                username = "kganger"
+                password="c918de12-f546-44ce-a321-73c393284a42"
+            }
+            image = "kganger/k3sdemo"
+        }
+    }
+}
